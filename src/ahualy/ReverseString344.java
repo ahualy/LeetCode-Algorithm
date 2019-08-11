0e9557f71dd1e2bb139d72a97be7566798a5e25f
ahualy <15336014045@163.com>
2019-08-11 10:18:22 +0800

    "LeetCode算法题20190811--更新解法"

--- src/ahualy/ReverseString344.java
+++ src/ahualy/ReverseString344.java
@@ -9,6 +9,7 @@ public class ReverseString344 {
     }
     public static void reverseString(char[] s){
 
+  // 解法一
         int n = s.length;
         for (int i = 0; i < n / 2; ++i) {
             int j = n - 1 - i;
@@ -18,6 +19,12 @@ public class ReverseString344 {
 
         }
         System.out.println(s);
+        //解法二
+       /* int length = s.length; for(int i=0; i<length/2; i++){
+            char temp = s[i];
+            s[i] = s[length-i-1];
+            s[length-i-1] = temp;
+        }*/
     }
 
 }
