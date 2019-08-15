310bde59ba2e5b7ff307d2f12034fb16cfe11c9c
ahualy <15593380565@163.com>
2019-08-15 19:26:09 +0800

    "LeetCode算法题20190815--更新注释"

--- src/ahualy/AddBinary67.java
+++ src/ahualy/AddBinary67.java
@@ -5,6 +5,9 @@ public class AddBinary67 {
 
         String a = "111";
         String b = "11";
+//      数字和数字字符进行运算
+//        System.out.println(('0'-'0')+1);
+//        System.out.println('0'+1);
         String res = addBinary(a,b);
         System.out.println(res);
 
@@ -17,15 +20,20 @@ public class AddBinary67 {
         while (i >= 0 || j >= 0) {
             int sum = carry;
             if (j >= 0)
+//              j--巧妙用法, -'0'的目的就是为了将数字字符转化为数组
                 sum += b.charAt(j--) - '0';
             if (i >= 0)
                 sum += a.charAt(i--) - '0';
-
+            System.out.println(sum);
+//          核心算法
             sb.append(sum % 2);
+//          更新进位
             carry = sum / 2;
         }
+//       最后判断是不是有进位
         if (carry != 0)
             sb.append(carry);
+//       最后将字符串做一个反转即可
         return sb.reverse().toString();
 
     }
