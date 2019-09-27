a06843fe72adc059c91084b6839116d37e2357cd
ahualy <15336014045@163.com>
2019-09-27 23:24:10 +0800

    删除class文件

---
+++ src/collection/Test.java
@@ -0,0 +1,18 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * default  可以写方法体了
+ */
+public class Test {
+    public static void main(String args[]){
+        List list = new ArrayList();
+        list.add("剃须刀");
+        list.add("皮鞋");
+        list.add("口红");
+
+
+    }
+}
